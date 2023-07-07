64c2e4aef56342c57d9c56b8a101fc6ae48d78e5
DDHLong <hoanglonghoab@gmail.com>
2023-07-07 18:09:48 +0700

    Update _layout.js

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,10 +1,29 @@
-import { Tabs } from "expo-router"
+import { Tabs } from "expo-router";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default () => {
-    return (
-        <Tabs>
-            <Tabs.Screen name="home" />
-            <Tabs.Screen name="form" />
-        </Tabs>
-    )
-}
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+          tabBarLabelStyle: { display: "none" },
+          title: "",
+        }}
+      />
+      <Tabs.Screen
+        name="form"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="plus" color={color} size={size} />
+          ),
+          tabBarLabelStyle: { display: "none" },
+          title: "",
+        }}
+      />
+    </Tabs>
+  );
+};
